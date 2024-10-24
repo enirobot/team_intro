b33d0e5b08ba52d4925cd4c08337cba693a2ab38
enirobot <enirobot@naver.com>
2024-10-25 08:48:15 +0900

    필요없는 코드 정리

--- lib/screens/main_menu_page.dart
+++ lib/screens/main_menu_page.dart
@@ -185,32 +185,3 @@ class MainMenuPage extends StatelessWidget {
         ));
   }
 }
-
-// class MainMenuPage extends StatelessWidget {
-//   final List<PageInfo> pages;
-//   final Function(int) onPageSelected;
-
-//   const MainMenuPage({
-//     super.key,
-//     required this.pages,
-//     required this.onPageSelected,
-//   });
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Center(
-//       child: Column(
-//         mainAxisAlignment: MainAxisAlignment.center,
-//         children: [
-//           for (int i = 0; i < pages.length; i++) ...[
-//             if (i > 0) const SizedBox(height: AppConstants.spaceBetweenButtons),
-//             MenuButton(
-//               text: pages[i].title,
-//               onPressed: () => onPageSelected(i + 1),
-//             ),
-//           ],
-//         ],
-//       ),
-//     );
-//   }
-// }
